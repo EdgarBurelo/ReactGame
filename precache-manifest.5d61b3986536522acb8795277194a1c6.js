e1a4ae2581b78585c2d8795a42bce5cb8d4aaaa3
Edgar Burelo <edgar.burelo@gmail.com>
2019-01-10 13:37:40 -0600

    Updates

---
+++ precache-manifest.5d61b3986536522acb8795277194a1c6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a517b9291f5f4dba9b9",
+    "url": "/ReactGame/static/css/main.22432052.chunk.css"
+  },
+  {
+    "revision": "6a517b9291f5f4dba9b9",
+    "url": "/ReactGame/static/js/main.6a517b92.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/ReactGame/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "493e1f321c4cba85ffe7",
+    "url": "/ReactGame/static/js/runtime~main.493e1f32.js"
+  },
+  {
+    "revision": "f3484a02924f1affef2af270b454b421",
+    "url": "/ReactGame/static/media/asteroids.f3484a02.jpg"
+  },
+  {
+    "revision": "03505d2ba6d18b00dfb6bf0329dfbc3c",
+    "url": "/ReactGame/static/media/logo1.03505d2b.png"
+  },
+  {
+    "revision": "c3e4b3e44ab79a65faaaff4c03deda70",
+    "url": "/ReactGame/static/media/logo10.c3e4b3e4.png"
+  },
+  {
+    "revision": "83ca326b711034c3a08a45942c6f89fd",
+    "url": "/ReactGame/static/media/logo11.83ca326b.png"
+  },
+  {
+    "revision": "a59146f2346cb44e338e9977b1f9b32c",
+    "url": "/ReactGame/static/media/logo12.a59146f2.png"
+  },
+  {
+    "revision": "926c3f2b5bfa7715a3ae93f35d920225",
+    "url": "/ReactGame/static/media/logo13.926c3f2b.png"
+  },
+  {
+    "revision": "1e687a12dee44df42ac1b5a818cf7e3b",
+    "url": "/ReactGame/static/media/logo2.1e687a12.png"
+  },
+  {
+    "revision": "11e95931c02d9ef07b4f03d7f8b79402",
+    "url": "/ReactGame/static/media/logo3.11e95931.png"
+  },
+  {
+    "revision": "3e68005eb567db103555bfe0c1df9fef",
+    "url": "/ReactGame/static/media/logo4.3e68005e.png"
+  },
+  {
+    "revision": "b1f168f6252b72e005ab9eb57450cc3d",
+    "url": "/ReactGame/static/media/logo5.b1f168f6.png"
+  },
+  {
+    "revision": "404baaf6de855996556548704fbc7c24",
+    "url": "/ReactGame/static/media/logo6.404baaf6.png"
+  },
+  {
+    "revision": "78a74326bf554311a77b45032fe1b447",
+    "url": "/ReactGame/static/media/logo7.78a74326.png"
+  },
+  {
+    "revision": "576c0278b51100dd2b9c63fa9b3cfd85",
+    "url": "/ReactGame/static/media/logo8.576c0278.png"
+  },
+  {
+    "revision": "fb83eb3e58c62713b1b97a52a899e2bb",
+    "url": "/ReactGame/static/media/logo9.fb83eb3e.png"
+  },
+  {
+    "revision": "3cd7389b23426bb5d653bd45412fa93c",
+    "url": "/ReactGame/static/media/chalkboard.3cd7389b.jpg"
+  },
+  {
+    "revision": "16168a03a96fde544971ae65a81df402",
+    "url": "/ReactGame/index.html"
+  }
+];
